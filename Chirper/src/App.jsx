d8c5467348fc9401036057b6959d2f5caa08c422
RajveerNarang <dhanurrajveer@gmail.com>
2023-06-18 13:33:57 +0530

    Done Till Sidebar

--- Chirper/src/App.jsx
+++ Chirper/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    // using BEM Naming Convention
+    <div className="app">
       <h1>Chirper 2.0</h1>
+
+      {/*Sidebar */}
+      <Sidebar />
+
+      {/* Chirper Feed */}
+      {/*Widgets */}
     </div>
   );
 }
---
+++ Chirper/src/components/Sidebar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Sidebar.css";
+import ForumIcon from "@mui/icons-material/Forum";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from "@mui/material";
+
+const Sidebar = () => {
+  return (
+    <>
+      {/* <div>Sidebar</div> */}
+      <div className="sidebar">
+        {/*Twitter icon */}
+        <ForumIcon />
+
+        {/*SidebarOption Component */}
+        <SidebarOption active Icon={HomeIcon} text="Home" />
+        {/*SidebarOption Component */}
+        <SidebarOption Icon={SearchIcon} text="Explore" />
+        {/*SidebarOption Component */}
+        <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+        {/*SidebarOption Component */}
+        <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+        {/*SidebarOption Component */}
+        <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+        {/*SidebarOption Component */}
+        <SidebarOption Icon={ListAltIcon} text="Lists" />
+        {/*SidebarOption Component */}
+        <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+        {/*SidebarOption Component */}
+        <SidebarOption Icon={MoreHorizIcon} text="More" />
+
+        {/*Button -> Chirp */}
+        <Button variant="outlined" className="sidebarChirpBtn" fullWidth>
+          Chirp
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
